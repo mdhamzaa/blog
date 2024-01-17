e8b6cfecbb15438ee9654f7ce01d5d4bb461cdfb
HamzaHadis <hamzahadis@users.noreply.github.com>
2024-01-17 12:12:58 +0530

    frontend added

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(
       "http://localhost:3002",
       "http://localhost:3003",
       "https://oneshotblog.netlify.app",
-      "https://main--oneshotblog.netlify.app/".
+      "https://main--oneshotblog.netlify.app/",
     ],
     credentials: true,
   })
